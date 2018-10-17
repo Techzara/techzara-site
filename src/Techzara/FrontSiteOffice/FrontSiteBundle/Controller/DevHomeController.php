83cfb0522877a532c90d2df7208260af20ef69a9
julien2901 <julien@livenexx.fr>
2018-10-17 11:32:23 +0300

    modification bo

--- src/Techzara/FrontSiteOffice/FrontSiteBundle/Controller/DevHomeController.php
+++ src/Techzara/FrontSiteOffice/FrontSiteBundle/Controller/DevHomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Techzara\FrontSiteOffice\FrontSiteBundle\Controller;
 
-use App\Techzara\Service\MetierManagerBundle\Utils\CmsName;
 use App\Techzara\Service\MetierManagerBundle\Utils\RoleName;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Techzara\Service\MetierManagerBundle\Utils\ServiceName;
@@ -20,13 +19,11 @@ class DevHomeController extends Controller
     public function indexAction()
     {
         // Récupérer manager
-        $_cms_manager            = $this->get(ServiceName::SRV_METIER_CMS);
         $_slide_manager          = $this->get(ServiceName::SRV_METIER_SLIDE);
         $_portfolio_manager      = $this->get(ServiceName::SRV_METIER_PORTFOLIO);
         $_portfolio_type_manager = $this->get(ServiceName::SRV_METIER_PORTFOLIO_TYPE);
         $_membres_liste          = $this->get(ServiceName::SRV_METIER_MEMBRES);
 
-        $_cms             = $_cms_manager->getDevCmsById(CmsName::ID_CMS_ACCUEIL);
         $_slides          = $_slide_manager->getAllDevSlide();
         $_portfolios      = $_portfolio_manager->getAllDevPortfolio();
         $_portfolio_types = $_portfolio_type_manager->getAllDevPortfolioType();
@@ -34,7 +31,6 @@ class DevHomeController extends Controller
 
 
         return $this->render('FrontSiteBundle:DevHome:index.html.twig', array(
-            'cms'             => $_cms,
             'slides'          => $_slides,
             'portfolios'      => $_portfolios,
             'portfolio_types' => $_portfolio_types,
@@ -49,14 +45,11 @@ class DevHomeController extends Controller
     public function siteAction()
     {
         // Récupérer manager
-        $_cms_manager   = $this->get(ServiceName::SRV_METIER_CMS);
         $_slide_manager = $this->get(ServiceName::SRV_METIER_SLIDE);
 
-        $_cms    = $_cms_manager->getDevCmsById(CmsName::ID_CMS_SITE);
         $_slides = $_slide_manager->getAllDevSlide();
 
         return $this->render('FrontSiteBundle:DevHome:site.html.twig', array(
-            'cms'    => $_cms,
             'slides' => $_slides
         ));
     }
@@ -68,12 +61,9 @@ class DevHomeController extends Controller
     public function confianceAction()
     {
         // Récupérer manager
-        $_cms_manager = $this->get(ServiceName::SRV_METIER_CMS);
 
-        $_cms = $_cms_manager->getDevCmsById(CmsName::ID_CMS_CONFIANCE);
 
         return $this->render('FrontSiteBundle:DevHome:confiance.html.twig', array(
-            'cms' => $_cms
         ));
     }
 
@@ -84,14 +74,11 @@ class DevHomeController extends Controller
     public function applicationAction()
     {
         // Récupérer manager
-        $_cms_manager   = $this->get(ServiceName::SRV_METIER_CMS);
         $_slide_manager = $this->get(ServiceName::SRV_METIER_SLIDE);
 
-        $_cms    = $_cms_manager->getDevCmsById(CmsName::ID_CMS_APPLICATION);
         $_slides = $_slide_manager->getAllDevSlide();
 
         return $this->render('FrontSiteBundle:DevHome:applications.html.twig', array(
-            'cms'    => $_cms,
             'slides' => $_slides
         ));
     }
@@ -103,12 +90,8 @@ class DevHomeController extends Controller
     public function ccmAction()
     {
         // Récupérer manager
-        $_cms_manager = $this->get(ServiceName::SRV_METIER_CMS);
-
-        $_cms = $_cms_manager->getDevCmsById(CmsName::ID_CMS_CCM);
 
         return $this->render('FrontSiteBundle:DevHome:ccm.html.twig', array(
-            'cms' => $_cms
         ));
     }
 
