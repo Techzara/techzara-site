7ea31f91244062ebc8fc094d6b3dbc52ce582df8
julkwel <julienrajerison5@gmail.com>
2019-01-11 23:25:15 +0300

    amelioration

--- public/front_site_office/tznew/js/main.js
+++ public/front_site_office/tznew/js/main.js
@@ -145,5 +145,14 @@
 		stickyBanner();
 	});
 
+	$('a[href^="#"]').on('click', function(event) {
+		var target = $( $(this).attr('href') );
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top - 200
+			}, 800);
+		}
+	});
 
 }());
